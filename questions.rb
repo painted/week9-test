9e83903be12fa14307b7b22f101d9e43a4f9ada4
Painted <will.allen@paintedltd.co.uk>
2014-07-04 11:55:07 +0100

    started friday test

--- questions.rb
+++ questions.rb
@@ -1,9 +1,14 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+	# array.sort.take_while do |string|
+	# 	string.start_with?('a') 
+	# end
+	array.delete_if{|string| !string.start_with?('a')}
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+	array.delete_if{|string| !string.start_with?('a', 'e', 'i', 'o', 'u')}
 end
 
 # remove instances of nil (but NOT false) from an array
