50b6198dd1365abcb8b9484a74c4cdc3a328cc5b
Painted <will.allen@paintedltd.co.uk>
2014-07-04 12:09:09 +0100

    resolved 5 tests

--- questions.rb
+++ questions.rb
@@ -24,6 +24,7 @@ end
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+	array.each { |w| w.reverse!}
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
